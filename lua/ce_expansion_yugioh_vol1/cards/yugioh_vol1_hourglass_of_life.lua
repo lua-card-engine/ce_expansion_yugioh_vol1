2549d115a0f1bb127427f395e4f4b75f39efbc7f
luttje <2738114+luttje@users.noreply.github.com>
2026-08-10 19:10:42 +0200

    Format cards

--- lua/ce_expansion_yugioh_vol1/cards/yugioh_vol1_hourglass_of_life.lua
+++ lua/ce_expansion_yugioh_vol1/cards/yugioh_vol1_hourglass_of_life.lua
@@ -5,37 +5,37 @@ CARD.Texture = "card_engine/expansions/ce_expansion_yugioh_vol1/hourglass_of_lif
 CARD.RearTexture = "card_engine/expansions/ce_expansion_yugioh_vol1/back"
 CARD.CardSize = CardEngine.DEFAULT_CARD_MODELS.JAPANESE_ROUNDED
 CARD.Attributes = {
-  Id = 8783685,
-  Typeline = {
-    "Fairy",
-    "Normal"
-  },
-  Type = "Normal Monster",
-  HumanReadableCardType = "Normal Monster",
-  FrameType = "normal",
-  Desc = "This creature grants power instead of shortening life.",
-  Race = "Fairy",
-  Atk = 700,
-  Def = 600,
-  Level = 2,
-  Attribute = "LIGHT",
-  YgoprodeckUrl = "https://ygoprodeck.com/card/hourglass-of-life-743",
-  CardSets = {
-    {
-      SetName = "Speed Duel GX: Duel Academy Box",
-      SetCode = "SGX1-ENI01",
-      SetRarity = "Common",
-      SetRarityCode = "(C)",
-      SetPrice = "0"
-    }
-  },
-  CardPrices = {
-    {
-      CardmarketPrice = "0.09",
-      TcgplayerPrice = "0.14",
-      EbayPrice = "0.99",
-      AmazonPrice = "0.20",
-      CoolstuffincPrice = "0.79"
-    }
-  }
+	Id = 8783685,
+	Typeline = {
+		"Fairy",
+		"Normal"
+	},
+	Type = "Normal Monster",
+	HumanReadableCardType = "Normal Monster",
+	FrameType = "normal",
+	Desc = "This creature grants power instead of shortening life.",
+	Race = "Fairy",
+	Atk = 700,
+	Def = 600,
+	Level = 2,
+	Attribute = "LIGHT",
+	YgoprodeckUrl = "https://ygoprodeck.com/card/hourglass-of-life-743",
+	CardSets = {
+		{
+			SetName = "Speed Duel GX: Duel Academy Box",
+			SetCode = "SGX1-ENI01",
+			SetRarity = "Common",
+			SetRarityCode = "(C)",
+			SetPrice = "0"
+		}
+	},
+	CardPrices = {
+		{
+			CardmarketPrice = "0.09",
+			TcgplayerPrice = "0.14",
+			EbayPrice = "0.99",
+			AmazonPrice = "0.20",
+			CoolstuffincPrice = "0.79"
+		}
+	}
 }
--- lua/ce_expansion_yugioh_vol1/cards/yugioh_vol1_sectarian_of_secrets.lua
+++ lua/ce_expansion_yugioh_vol1/cards/yugioh_vol1_sectarian_of_secrets.lua
@@ -5,37 +5,37 @@ CARD.Texture = "card_engine/expansions/ce_expansion_yugioh_vol1/sectarian_of_sec
 CARD.RearTexture = "card_engine/expansions/ce_expansion_yugioh_vol1/back"
 CARD.CardSize = CardEngine.DEFAULT_CARD_MODELS.JAPANESE_ROUNDED
 CARD.Attributes = {
-  Id = 15507080,
-  Typeline = {
-    "Spellcaster",
-    "Normal"
-  },
-  Type = "Normal Monster",
-  HumanReadableCardType = "Normal Monster",
-  FrameType = "normal",
-  Desc = "This wizard worships the darkness, and is served by a sinister hand that drags its enemies into oblivion.",
-  Race = "Spellcaster",
-  Atk = 700,
-  Def = 500,
-  Level = 2,
-  Attribute = "DARK",
-  YgoprodeckUrl = "https://ygoprodeck.com/card/sectarian-of-secrets-1335",
-  CardSets = {
-    {
-      SetName = "Tournament Pack 8",
-      SetCode = "TP8-EN019",
-      SetRarity = "Common",
-      SetRarityCode = "(C)",
-      SetPrice = "5.22"
-    }
-  },
-  CardPrices = {
-    {
-      CardmarketPrice = "0.83",
-      TcgplayerPrice = "1.71",
-      EbayPrice = "3.99",
-      AmazonPrice = "5.39",
-      CoolstuffincPrice = "2.99"
-    }
-  }
+	Id = 15507080,
+	Typeline = {
+		"Spellcaster",
+		"Normal"
+	},
+	Type = "Normal Monster",
+	HumanReadableCardType = "Normal Monster",
+	FrameType = "normal",
+	Desc = "This wizard worships the darkness, and is served by a sinister hand that drags its enemies into oblivion.",
+	Race = "Spellcaster",
+	Atk = 700,
+	Def = 500,
+	Level = 2,
+	Attribute = "DARK",
+	YgoprodeckUrl = "https://ygoprodeck.com/card/sectarian-of-secrets-1335",
+	CardSets = {
+		{
+			SetName = "Tournament Pack 8",
+			SetCode = "TP8-EN019",
+			SetRarity = "Common",
+			SetRarityCode = "(C)",
+			SetPrice = "5.22"
+		}
+	},
+	CardPrices = {
+		{
+			CardmarketPrice = "0.83",
+			TcgplayerPrice = "1.71",
+			EbayPrice = "3.99",
+			AmazonPrice = "5.39",
+			CoolstuffincPrice = "2.99"
+		}
+	}
 }
